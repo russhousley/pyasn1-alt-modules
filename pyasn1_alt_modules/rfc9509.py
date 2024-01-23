300cd99dc7bdd3b6974d05c8dd6534066f696f53
russhousley <housley@vigilsec.com>
2024-01-23 09:39:11 -0500

    Add support for RFC 9509

---
+++ pyasn1_alt_modules/rfc9509.py
@@ -0,0 +1,24 @@
+#
+# This file is part of pyasn1-alt-modules software.
+#
+# Created by Russ Housley.
+#
+# Copyright (c) 2024, Vigil Security, LLC
+# License: http://vigilsec.com/pyasn1-alt-modules-license.txt
+#
+# Extended Key Usage (EKU) for 5G Network Functions
+#
+# ASN.1 source from:
+# https://www.rfc-editor.org/rfc/rfc9509.txt
+#
+
+from pyasn1.type import univ
+
+
+id_kp = univ.ObjectIdentifier('1.3.6.1.5.5.7.3')
+
+id_kp_jwt = id_kp + (37,)
+
+id_kp_httpContentEncrypt = id_kp + (38,)
+
+id_kp_oauthAccessTokenSigning = id_kp + (39,)
