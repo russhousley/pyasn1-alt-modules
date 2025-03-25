1d51e21b9a57cf291a980edb4d415a616e02234c
russhousley <housley@vigilsec.com>
2025-03-25 01:25:22 -0400

    Add support for Python 3.13

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ Programming Language :: Python :: 3.9
 Programming Language :: Python :: 3.10
 Programming Language :: Python :: 3.11
 Programming Language :: Python :: 3.12
+Programming Language :: Python :: 3.13
 Topic :: Communications
 Topic :: Software Development :: Libraries :: Python Modules
 """
